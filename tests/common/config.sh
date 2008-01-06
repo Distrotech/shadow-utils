6cde8339cf52cb2cdb49ff3342cb7739d5d4e6d0
nekral-guest <nekral-guest@5a98b0ae-9ef6-0310-add3-de5d479b70d7>
2008-01-06 19:49:24 +0000

    Add helpers for an updated test infrastructure (handling of the chroot's configuration).

---
+++ tests/common/config.sh
@@ -0,0 +1,48 @@
+# Generic functions to save, change, and restore configuration files
+
+set -e
+
+
+# Save the configuration files in tmp.
+save_config ()
+{
+	[ ! -d tmp ] && mkdir tmp
+	find config -depth -type f | sed -e 's/config\///' |
+	while read file
+	do
+		mkdir -p "tmp/$(dirname "$file")"
+		[ -f "/$file" ] && cp -dp "/$file" "tmp/$file"
+	done
+}
+
+# Copy the config files from config to the system
+change_config ()
+{
+	find config -depth -type f | sed -e 's/config\///' |
+	while read file
+	do
+		cp -f "config/$file" "/$file"
+	done
+}
+
+# Restored the config files in the system.
+# The config files must be saved before with save_config ().
+restore_config ()
+{
+	find config -depth -type f | sed -e 's/config\///' |
+	while read file
+	do
+		[ -f "tmp/$file" ] && \
+			cp -dp "tmp/$file" "/$file" && \
+			rm "tmp/$file"
+		d="$(dirname "tmp/$file")"
+		while [ -n "$d" ] && [ "$d" != "." ]
+		do
+			rmdir "$d" 2>/dev/null || true
+			d="$(dirname "$d")"
+		done
+	done
+
+	rmdir tmp 2>/dev/null || true
+}
+
