7ca179b885d2eb2a781860ab3889e1b39e24e331
nekral-guest <nekral-guest@5a98b0ae-9ef6-0310-add3-de5d479b70d7>
2012-02-04 16:07:22 +0000

    Add 24 tests for the --root options

--- tests/common/config.sh
+++ tests/common/config.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+build_path=/root/build/shadow-4.1.5/
 
 # Save the configuration files in tmp.
 save_config ()
@@ -47,5 +48,73 @@ restore_config ()
 	done
 
 	rmdir tmp 2>/dev/null || true
+}
+
+prepare_chroot ()
+{
+	mkdir tmp/root
+	cp -rfdp config_chroot/* tmp/root/
+
+	lists=/root/tests/common/config_chroot.list
+	[ -f config_chroot.list ] && lists="$lists config_chroot.list"
+	cat $lists | grep -v "#" | while read f
+	do
+		# Create parent directory if needed
+		d=$(dirname tmp/root/$f)
+		[ -d $d ] || mkdir -p $d
+		# Create hard link
+		ln $f tmp/root/$f
+	done
+
+	# Copy existing gcda
+	mkdir -p tmp/root$build_path/lib
+	mkdir -p tmp/root$build_path/libmisc
+	mkdir -p tmp/root$build_path/src
+	find "$build_path" -name "*.gcda" | while read f
+	do
+		ln $f tmp/root/$f
+	done
+}
+
+clean_chroot ()
+{
+	# Remove copied files
+	lists=/root/tests/common/config_chroot.list
+	[ -f config_chroot.list ] && lists="$lists config_chroot.list"
+	cat $lists | grep -v "#" | while read f
+	do
+		rm -f tmp/root/$f
+		# Remove parent directory if empty
+		d=$(dirname tmp/root/$f)
+		rmdir -p --ignore-fail-on-non-empty $d
+	done
+
+	find "$build_path" -name "*.gcda" | while read f
+	do
+		rm -f tmp/root/$f
+	done
+	find tmp/root -name "*.gcda" | while read f
+	do
+		g=${f#tmp/root}
+		mv "$f" "$g"
+	done
+	rmdir tmp/root$build_path/lib
+	rmdir tmp/root$build_path/libmisc
+	rmdir tmp/root$build_path/src
+	rmdir tmp/root$build_path
+	rmdir tmp/root/root/build
+	rmdir tmp/root/root
+
+	find config_chroot -type f | while read f
+	do
+		f=${f#config_chroot/}
+		rm -f tmp/root/$f
+	done
+
+	find config_chroot -depth -type d | while read d
+	do
+		d=${d#config_chroot}
+		rmdir tmp/root$d
+	done
 }
 
